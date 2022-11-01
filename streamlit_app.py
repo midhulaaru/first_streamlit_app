4a977081f845514faec8fb919878634c6a8ab01d
midhulaaru <117166362+midhulaaru@users.noreply.github.com>
2022-11-01 19:22:15 +0530

    Update streamlit_app.py
    added fruits dataframe from file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 streamlit.title("Mr Aru's Kitchen")  
 streamlit.header("Breakfast Favourites")
 streamlit.text("🍞 Cheese Bread Omlette")
@@ -8,3 +10,7 @@ streamlit.text("🥑 Avacado Juice")
 streamlit.text("🥗 Broccoli Soup")
 streamlit.text("🐔 Hard Boied Egg")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
+
