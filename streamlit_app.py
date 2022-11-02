4b4e1db5cec4c85419452dff321753f7c47e48bc
midhulaaru <117166362+midhulaaru@users.noreply.github.com>
2022-11-02 23:02:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #Dispay only user selected fruits 
 streamlit.dataframe(fruits_to_show)
 
-
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 
